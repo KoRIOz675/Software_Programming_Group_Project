a5e5a216a443cd1a306c9448d514ed4de25c85fd
Beringer Guilhem <101707901+onariel@users.noreply.github.com>
2026-09-24 11:27:50 +0900

    MEDIUM: ci: added stats to charaters, readme.md changed to explain the stats system

--- SPECIFICATION.md
+++ SPECIFICATION.md
@@ -24,6 +24,14 @@
 - The player choose the name of the hero
 - The player gets points to separate in different statistics
 
+## Combat mechanics
+
+- turned base combat with initiative to determine the order of the round
+- Dice based attack damage
+- Armor class defense, if the result of the attacking roll is inferior to the defense the attack misses, else it hit with full damage of the weapon
+- Three types of damage, strength weapon, dexterity weapon, magic spells.
+- Each class will be link to a single type of damage.
+
 ## Art Style
 
 The sprites and art styles are from [Anokolisa on itch.io](https://anokolisa.itch.io/)
--- data/characters/__init__.py
+++ data/characters/__init__.py
@@ -4,10 +4,30 @@ from data.window_renderer import scale_image, render
 
 class Character:
     IMAGE_PATH = None
+    strength = 0
+    dexterity = 0
+    constitution = 0
+    intelligence = 0
+    wisdom = 0
+    charisma = 0
 
     def __init__(self, position):
         self.image = scale_image(image.load(self.IMAGE_PATH))
         self.position = position
+        self.strength = 10
+        self.dexterity = 10
+        self.constitution = 10
+        self.intelligence = 10
+        self.wisdom = 10
+        self.charisma = 10
 
     def draw(self):
         render(self.image, self.position)
+
+    def set_stats(self, strength, dexterity, constitution, intelligence, wisdom, charisma):
+        self.strength = strength
+        self.dexterity = dexterity
+        self.constitution = constitution
+        self.intelligence = intelligence
+        self.wisdom = wisdom
+        self.charisma = charisma
