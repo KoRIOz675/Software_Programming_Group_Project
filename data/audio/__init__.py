db476f26e5205eeb9765cd4730e4966078903a94
KoRIOz675 <thomas@mafille.net>
2026-09-25 11:58:45 +0900

    MEDIUM: ci: Updated audio behaviour and callouts

--- data/audio/__init__.py
+++ data/audio/__init__.py
@@ -1,5 +1,4 @@
-from pygame import mixer
-from random import uniform
+from pygame import *
 from enum import Enum
 
 class AudioType(Enum):
@@ -7,28 +6,23 @@ class AudioType(Enum):
     SFX = "sfx"
 
 class Audio:
-    AUDIO_FILE = None
-    AUDIO_VOLUME = 1.0
-    AUDIO_TYPE = AudioType.BGM
-
-    def __init__(self):
+    def __init__(self, AUDIO_FILE: str, AUDIO_VOLUME: float, AUDIO_TYPE: AudioType):
         if not mixer.get_init():
             mixer.init()
         self.sound = None
-        self.channel = None
-        if self.AUDIO_FILE is not None:
-            self.set_music(self.AUDIO_FILE)
+        self.AUDIO_FILE = AUDIO_FILE
+        self.AUDIO_VOLUME = AUDIO_VOLUME
+        self.AUDIO_TYPE = AUDIO_TYPE
+        self.set_music(self.AUDIO_FILE)
         self.set_volume(self.AUDIO_VOLUME)
 
-    def play(self, loops=None):
-        if self.AUDIO_FILE is None:
-            raise RuntimeError("No audio file set. Call set_music() first.")
+    def play(self, loops: int = None):
         if self.AUDIO_TYPE == AudioType.BGM:
             mixer.music.load(self.AUDIO_FILE)
             mixer.music.set_volume(self.AUDIO_VOLUME)
             mixer.music.play(-1 if loops is None else loops)
         else:
-            self.channel = self.sound.play(0 if loops is None else loops)
+            self.sound.play(0 if loops is None else loops)
 
     def stop(self):
         if self.AUDIO_TYPE == AudioType.BGM:
@@ -39,14 +33,10 @@ class Audio:
     def pause(self):
         if self.AUDIO_TYPE == AudioType.BGM:
             mixer.music.pause()
-        elif self.channel is not None:
-            self.channel.pause()
 
     def unpause(self):
         if self.AUDIO_TYPE == AudioType.BGM:
             mixer.music.unpause()
-        elif self.channel is not None:
-            self.channel.unpause()
 
     def set_music(self, music_file: str):
         self.AUDIO_FILE = music_file
@@ -60,14 +50,3 @@ class Audio:
             mixer.music.set_volume(self.AUDIO_VOLUME)
         elif self.sound is not None:
             self.sound.set_volume(self.AUDIO_VOLUME)
-
-    def set_audio_type(self, audio_type):
-        try:
-            self.AUDIO_TYPE = AudioType(audio_type)
-        except ValueError:
-            valid = [t.value for t in AudioType]
-            raise ValueError(f"Invalid audio type: {audio_type}. Must be one of {valid}.")
-        self.sound = None
-        self.channel = None
-        if self.AUDIO_FILE is not None:
-            self.set_music(self.AUDIO_FILE)
--- data/scenes/__init__.py
+++ data/scenes/__init__.py
@@ -1,13 +1,20 @@
-from pygame import image
+from pygame import *
 
+from data.audio import Audio, AudioType
 from data.window_renderer import scale_image, render
 
 class Scene:
-    IMAGE_PATH = None
-
-    def __init__(self, position=(0, 0)):
+    def __init__(self, IMAGE_PATH: str, AUDIO_FILE: str, AUDIO_VOLUME: float):
+        self.IMAGE_PATH = IMAGE_PATH
         self.image = scale_image(image.load(self.IMAGE_PATH))
-        self.position = position
+        self.position = (0, 0)
+        self.music = Audio(AUDIO_FILE, AUDIO_VOLUME, AudioType.BGM)
 
     def draw(self):
         render(self.image, self.position)
+
+    def play_music(self):
+        self.music.play(-1)
+    
+    def stop_music(self):
+        self.music.stop()
